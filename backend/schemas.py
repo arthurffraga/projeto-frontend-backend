58df2700ee08cf87d91e9908e6e6a1d71e12057d
arthurffraga <arthurfragaa00@gmail.com>
2026-05-19 17:23:56 -0300

    schamas parciamente pronto

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,6 +1,11 @@
 from typing import Optional
 from pydantic import BaseModel, ConfigDict
+from enum import Enum
 
+class Unidade(str, Enum):
+    ml = "ml"
+    mg = "mg"
+    g = "g"
 class CategoriaCreate(BaseModel):
     nome: str
 
@@ -10,3 +15,25 @@ class CategoriaUpdate(BaseModel):
 class CategoriaResponse(BaseModel):
     id: int
     nome: str
+
+class MedicamentoCreate(BaseModel):
+    nome: str
+    preco: float
+    quantidade: int
+    unidade: Unidade
+    categoria_id: int
+
+class MedicamentoUpdate(BaseModel):
+    nome: Optional[str] = None
+    preco: Optional[float] = None
+    quantidade: Optional[int] = None
+    unidade: Optional[Unidade] = None
+    categoria_id: Optional[int] = None
+
+class MedicamentoResponse(BaseModel):
+    id: int
+    nome: str
+    preco: float
+    quantidade: int
+    unidade: Unidade
+    categoria_id: int
